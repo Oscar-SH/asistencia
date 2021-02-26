de8018d9ccadb73bb3b035221b5ffe21cb9d78bd
Oscar-SH <oscrsnts29@gmail.com>
2021-02-25 22:03:42 -0600

    create migrationsss

---
+++ database/migrations/2021_02_26_034924_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('namet', 50);
+            $table->string('lnamet', 100);
+            $table->string('horario', 50);
+            $table->string('contacto', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
